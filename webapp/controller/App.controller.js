2456f0a19fce369864dd43b92ee6b66ff04b361f
P2000177894 <vasavadadhruvisha@gmail.com>
2018-06-27 06:49:55 +0000

    Step 5: Controllers

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -4,6 +4,9 @@ sap.ui.define([
 	"use strict";
 
 	return Controller.extend("com.synconic.practics.SAPUI5_Walkthrough.controller.App", {
-
+		onShowHello : function () {
+         // show a native JavaScript alert
+         alert("Hello World");
+      }
 	});
 });
